243d3205b9d35c40723795bc2cd92ce070911c17
Mickael-95 <72389929+Mickael-95@users.noreply.github.com>
2023-05-04 15:45:15 +0200

    Change LED color on button press

--- socket/server.js
+++ socket/server.js
@@ -57,36 +57,67 @@ xbeeAPI.parser.on("data", function (frame) {
     console.log(">> ZIGBEE_RECEIVE_PACKET >", dataReceived);
 
   }
-
   if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
     // let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
     console.log("NODE_IDENTIFICATION");
     console.log(frame);
 
-    const client = mqtt.connect('mqtt://test.mosquitto.org');
+    const client = mqtt.connect('ws://broker.emqx.io:8083/mqtt');
 
-    client.on('connect', function(){
-      client.subscribe('/player/name', function(err){
-        if(!err){
+    client.on('connect', function () {
+      client.subscribe('/player/name', function (err) {
+        if (!err) {
           client.publish('/player/name', frame.nodeIdentifier)
         }
       })
     });
-    
-    // client.on('message', function(topic, player){
-    //   console.log(topic, player);
-    //   client.end()
-    // })
+
+    client.on('message', function(topic, player){
+      console.log(topic, player);
+      client.end()
+    })
     //storage.registerSensor(frame.remote64)
 
   } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
+    const timestamp = new Date().getTime();
+    console.log("ZIGBEE_IO_DATA_SAMPLE_RX - " + timestamp)
+    // console.log(frame)
 
-    console.log("ZIGBEE_IO_DATA_SAMPLE_RX")
-    console.log(frame)
+    //Envoyer au topic le NI/MAC de qui a appuyé sur le bouton + le timestamp
+    
+    const { DIO1, DIO2, DIO3, DIO4 } = frame.digitalSamples;
+
+    const turn_red = { // AT Request to be sent
+      type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+      destination64: "FFFFFFFFFFFFFFFF",
+      command: "D2",
+      commandParameter: [],
+    };
+
+    const turn_green = { // AT Request to be sent
+      type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+      destination64: frame.remote64,
+      command: "D3",
+      commandParameter: [],
+    };
+
+    const turn_blue = { // AT Request to be sent
+      type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+      destination64: frame.remote64,
+      command: "D4",
+      commandParameter: [],
+    };
+
+
+
+    xbeeAPI.builder.write(turn_blue);
+    xbeeAPI.builder.write(turn_red);
+
     //storage.registerSample(frame.remote64,frame.analogSamples.AD0 )
 
   } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
-    console.log("REMOTE_COMMAND_RESPONSE")
+    const timestamp = new Date().getTime();
+    console.log("REMOTE_COMMAND_RESPONSE - " + timestamp)
     console.log(frame);
     let dataReceived = String.fromCharCode.apply(null, frame.commandData)
     console.log(dataReceived);
